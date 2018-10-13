7bd02412af12225c2011a9c1680220aca5f3b699
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-13 01:02:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,4 +73,8 @@ class Owner
     @pets[:fishes] = []
   end
 
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end 
+
 end
